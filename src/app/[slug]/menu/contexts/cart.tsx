0a5980a012348a26b37527180dd5a54520d6c485
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2025-02-23 10:19:17 -0300

    feat: ensure cart product quantity can be increased

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -14,6 +14,7 @@ export interface ICartContext {
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -22,6 +23,7 @@ export const CartContext = createContext<ICartContext>({
   toggleCart: () => { },
   addProduct: () => { },
   decreaseProductQuantity: () => { },
+  increaseProductQuantity: () => { },
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -52,19 +54,28 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   };
 
   const decreaseProductQuantity = (productId: string) => {
-    setProducts(prevProducts => {
-      return prevProducts.map(prevProduct => {
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
         if (prevProduct.id !== productId) {
           return prevProduct;
         }
-
         if (prevProduct.quantity === 1) {
           return prevProduct;
         }
-        return { ...prevProduct, quantity: prevProduct.quantity - 1 }
+        return { ...prevProduct, quantity: prevProduct.quantity - 1 };
       })
     })
   }
+  const increaseProductQuantity = (productId: string) => {
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id !== productId) {
+          return prevProduct;
+        }
+        return { ...prevProduct, quantity: prevProduct.quantity + 1 };
+      });
+    });
+  };
   return (
     <CartContext.Provider
       value={{
@@ -73,6 +84,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         toggleCart,
         addProduct,
         decreaseProductQuantity,
+        increaseProductQuantity,
       }}
     >
       {children}
